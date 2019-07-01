05cb1dd904ca92038ba7b1640caae677b7129568
Charanjit Singh <bhallacharanjit@gmail.com>
2019-07-01 11:24:13 +0530

    Ionic 1 July

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['list.page.scss']
 })
 export class ListPage implements OnInit {
+  public form = [
+    { val: 'ABC', isChecked: true , textshow:'axys'},
+    { val: 'Sausage', isChecked: false , textshow:'axys1'},
+    { val: 'Mushroom', isChecked: false, textshow:'axys2' },
+    { val: 'Mushroom2', isChecked: false , textshow:'axys3'}
+  ];
   private selectedItem: any;
   private icons = [
     'flask',
@@ -28,6 +34,11 @@ export class ListPage implements OnInit {
         icon: this.icons[Math.floor(Math.random() * this.icons.length)]
       });
     }
+  }
+
+  checkChanged(entry) {
+    console.log("Check changed");
+    alert('U changed checkbox '+entry.val);
   }
 
   ngOnInit() {
